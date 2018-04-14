4703df4b4d9e741079d6ff48f7628200d562e12d
William Lindblom <william.lindblom@softhouse.se>
2018-04-14 17:51:43 +0200

    Extract coordinates from response

--- app/src/services/kulturarv.service.ts
+++ app/src/services/kulturarv.service.ts
@@ -1,23 +1,21 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import * as xml2js from 'xml2js';
 
 @Injectable()
 export class KulturarvService {
   private url = "http://kulturarvsdata.se/ksamsok/api?method=search&x-api=test&query=boundingBox=/WGS84+%2215.588544384554211 56.17562894058903 15.609959141329114 56.19716749533957%22";
 
-  private parser = new DOMParser();
-  private xmlParser = new xml2js();
+  private domParser = new DOMParser();
   constructor(private http: HttpClient) {
 
   }
 
   getKulturarv(): void {
-    this.http.get(this.url).subscribe((response: string) => {
+    this.http.get(this.url).subscribe((response: any) => {
       console.log("Hello world");
-      console.log(response);
-      let dom = this.parser.parseFromString(response, "text/xml");
-      console.log(dom.getElementsByTagName("result")[0]);
+      console.log(response.result.records[0].record['@graph']);//['ns5:coordinates']);
+      let coordinateXml = response.result.records[0].record['@graph'].find(x => x['@type'] == 'ns5:Context')['ns5:coordinates']['@value'];
+      console.log(this.domParser.parseFromString(coordinateXml, "application/xml").childNodes[0].childNodes[0].innerHTML);
 
 
     }, (error2) => {
