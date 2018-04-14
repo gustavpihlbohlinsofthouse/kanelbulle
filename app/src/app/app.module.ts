ded37446240f0364b605e494c08f51e1377242fb
William Lindblom <william.lindblom@softhouse.se>
2018-04-14 17:17:02 +0200

    Work in progress

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {HttpClientModule} from "@angular/common/http";
+import {KulturarvService} from "../services/kulturarv.service";
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    KulturarvService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
